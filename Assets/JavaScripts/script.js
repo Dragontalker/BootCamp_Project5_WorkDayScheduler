372fc5e26e0d38301e499b7921931f22231b034a
Richard Yang <richard.yang.tong@gmail.com>
2021-01-27 21:23:26 -0500

    Added feature, adjust the color of input area based on current time.

--- Assets/JavaScripts/script.js
+++ Assets/JavaScripts/script.js
@@ -10,4 +10,36 @@ $(document).ready(() => {
             localStorage.setItem(time, event);
         })
     }
+
+    // Use Moment JS to get the time.
+    let currentHour = moment().format('H');
+    if (currentHour < 9) {
+        $('.past').addClass('future');
+        $('.past').removeClass('past');
+        $('.past').removeClass('present');
+    } else if (currentHour > 17) {
+        return;
+    } else {
+        // Set current time block red
+        let currentTimeBlock = `#Ipt-${currentHour}`;
+        $(currentTimeBlock).removeClass('past');
+        $(currentTimeBlock).removeClass('future');
+        $(currentTimeBlock).addClass('present');
+
+        // Set past time block grey
+        for (let i = 9; i < currentHour; i++) {
+            let pastTimeBlock = `#Ipt-${i}`;
+            $(pastTimeBlock).removeClass('present');
+            $(pastTimeBlock).removeClass('future');
+            $(pastTimeBlock).addClass('past');            
+        }
+
+        // future time block green
+        for (let i = (currentHour + 1); i < 18; i++) {
+            let futureTimeBlock = `#Ipt-${i}`;
+            $(futureTimeBlock).removeClass('present');
+            $(futureTimeBlock).removeClass('past');
+            $(futureTimeBlock).addClass('future');  
+        }
+    }
 })
